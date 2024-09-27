3db75d5ff1ddaf3e92ab3e44a18e6c49a83ed12f
lam-nguyen-topebox <127288099+lam-nguyen-topebox@users.noreply.github.com>
2024-09-28 00:47:42 +0700

    Add simple benchmark

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,9 +7,25 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+fn calculated_greet(name: &str) -> String {
+    let mut sum = 0;
+    let start_time = std::time::Instant::now();
+    for i in 0i64..100_000_000_000 {
+        sum = i;
+    }
+    format!(
+        "Hello, {}! You've been greeted from Rust! {} times!!! Time taken: {}ms",
+        name,
+        sum,
+        start_time.elapsed().as_millis()
+    )
+}
+
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![calculated_greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,13 @@ function App() {
 
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name: name() }));
+    setGreetMsg(await invoke("calculated_greet", { name: name() }));
+    const start = Date.now();
+    let sum = 0;
+    for (let i = 0; i < 100_000_000; i++) {
+      sum = i;
+    }
+    console.log(`Time taken: ${Date.now() - start}ms`);
   }
 
   return (
