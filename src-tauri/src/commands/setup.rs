d97af3bc9b0b37be86700c21add010bef296f0aa
bowber <46023302+bowber@users.noreply.github.com>
2024-12-02 15:08:37 +0700

    Implement app data directory creation

--- src-tauri/src/commands/setup.rs
+++ src-tauri/src/commands/setup.rs
@@ -1,7 +1,24 @@
-use crate::data::errors::DataError;
+use tokio::fs;
+
+use crate::{data::errors::DataError, paths::APP_DATA_PATH};
 
 #[tauri::command]
 pub async fn setup() -> Result<(), DataError> {
+    let app_data_path = match APP_DATA_PATH.get() {
+        Some(path) => path,
+        None => {
+            eprintln!("Error getting app data path");
+            return Err(DataError::PathError);
+        }
+    };
+    match fs::create_dir_all(app_data_path).await {
+        Ok(()) => Ok(()),
+        Err(e) => {
+            eprintln!("Error creating app data directory: {:?}", e);
+            Err(DataError::PathError)
+        }
+    }?;
+
     match crate::data::setup::try_init_db().await {
         Ok(()) => Ok(()),
         Err(e) => {
